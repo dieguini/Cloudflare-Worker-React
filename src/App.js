d8a87156c77a86a449d8e5ac5c9d2d352df8c592
G0l1at <diego.jauregui.salvatierra@gmail.com>
2024-03-25 01:15:08 +0000

    feat: add mui components from guide

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload asdasdsa.
         </p>
         <a
           className="App-link"
